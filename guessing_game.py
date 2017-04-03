7ede8741093e3d66ae4c3bd6e2e5cc3aaf0f2dae
jschoeni <juliaschoeni@yahoo.com>
2017-04-03 15:46:25 -0400

    Adding guessing game

---
+++ guessing_game.py
@@ -0,0 +1,33 @@
+import random
+
+# Game set up
+print("Welcome to the guessing game!")
+number_of_guesses = 3  # User has 3 guesses before losing the game
+user_won = False
+
+# Computer selects a random number between 1 and 10
+correct_answer = random.randint(1, 10)
+print(correct_answer)
+
+while number_of_guesses > 0:
+    # User guesses the number
+    user_guess = input("Guess my number: ")
+    user_guess = int(user_guess)
+
+    # Computer tells user whether the guess was too high or too low
+    if user_guess == correct_answer:
+        print("Good guess!")
+        print("You are correct!")
+        user_won = True
+        break
+    elif user_guess < correct_answer:
+        print("The correct answer is larger")
+    elif user_guess > correct_answer:
+        print("The correct answer is smaller")
+
+    number_of_guesses -= 1
+
+if user_won is True:
+    print("You won!")
+else:
+    print("You lost!")
