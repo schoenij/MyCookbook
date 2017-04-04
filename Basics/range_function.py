9e111eee756e4722c80a395fb193593ca85d2714
jschoeni <juliaschoeni@yahoo.com>
2017-04-04 12:16:07 -0400

    Adding information about lists

---
+++ Basics/range_function.py
@@ -0,0 +1,11 @@
+for i in range(5):
+    print(i)    # Starts at 0 and does not include 5
+
+for i in range(5, 10):  # Starts at 5 and does not include 10
+    print(i)
+
+for i in range(0, 10, 3):   # Starts at 0 goes to 10 and increases by increments of 3
+    print(i)
+
+for i in range(0, -10, -2):     # Counts backwards in increments of 2 from 0 to -10
+    print(i)
